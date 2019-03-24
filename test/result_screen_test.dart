5117b6e4dab5101630b17a66689e42f0b42bd30a
david <ariedov@gmail.com>
2019-03-24 16:46:01 +0200

    adds result screen test

---
+++ test/result_screen_test.dart
@@ -0,0 +1,80 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mansplaining/logic/screens.dart';
+import 'package:mansplaining/ui/result_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  testWidgets('Not mansplaining screen test', (tester) async {
+    final screen = NotMansplainingScreen();
+
+    var called = false;
+    await tester.pumpWidget(boilerplate(ResultScreenWidget(
+      screen: screen,
+      startOverCallback: (context) => called = true,
+    )));
+
+    final titleFinder = find.text(screen.title);
+    expect(titleFinder, findsOneWidget);
+
+    await tester.tap(find.byIcon(Icons.settings_backup_restore));
+    expect(called, true);
+  });
+
+  testWidgets('Probably mansplaining screen test', (tester) async {
+    final screen = ProbablyMansplainingScreen();
+
+    var called = false;
+    await tester.pumpWidget(boilerplate(ResultScreenWidget(
+      screen: screen,
+      startOverCallback: (context) => called = true,
+    )));
+
+    final titleFinder = find.text(screen.title);
+    expect(titleFinder, findsOneWidget);
+
+    await tester.tap(find.byIcon(Icons.settings_backup_restore));
+    expect(called, true);
+  });
+
+  testWidgets('Definitely mansplaining screen test', (tester) async {
+    final screen = DefinitelyMansplainingScreen();
+
+    var called = false;
+    await tester.pumpWidget(boilerplate(ResultScreenWidget(
+      screen: screen,
+      startOverCallback: (context) => called = true,
+    )));
+
+    final titleFinder = find.text(screen.title);
+    expect(titleFinder, findsOneWidget);
+
+    await tester.tap(find.byIcon(Icons.settings_backup_restore));
+    expect(called, true);
+  });
+
+  testWidgets('Stop now screen test', (tester) async {
+    final screen = StopNowScreen();
+
+    var called = false;
+    await tester.pumpWidget(boilerplate(ResultScreenWidget(
+      screen: screen,
+      startOverCallback: (context) => called = true,
+    )));
+
+    final titleFinder = find.text(screen.title);
+    expect(titleFinder, findsOneWidget);
+
+    await tester.tap(find.byIcon(Icons.settings_backup_restore));
+    expect(called, true);
+  });
+}
+
+Widget boilerplate(Widget screen) {
+  return Directionality(
+    child: MediaQuery(
+      data: MediaQueryData(),
+      child: screen,
+    ),
+    textDirection: TextDirection.ltr,
+  );
+}
